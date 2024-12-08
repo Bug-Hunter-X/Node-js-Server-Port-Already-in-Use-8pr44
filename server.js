047c6b8e0f69c3ebf1b090d585854f433784066d
BugHunter-X <askzenai@gmail.com>
2024-12-08 06:00:06 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+
+//Error: The server is listening on port 8080 but the port is already in use
